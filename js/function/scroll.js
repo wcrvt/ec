f05a6621df79511fa6c676b05754ff8f736c5a86
wcrvt <cat0127.k@gmail.com>
2021-11-03 03:09:42 +0900

    second commit

--- js/function/scroll.js
+++ js/function/scroll.js
@@ -1,6 +1,6 @@
 
-function scroll(box_id) {
-  const position_offset = document.getElementById('').getBoundingClientRect().top+window.pageYOffset;
+function scrollPage(box_id) {
+  const position_offset = document.getElementById('sec-viewpoint').getBoundingClientRect().top+window.pageYOffset;
   const element_top = document.getElementById(box_id).getBoundingClientRect().top;
   const scroll_offset=10;
   const position = element_top + window.pageYOffset - position_offset + scroll_offset;
